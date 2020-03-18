d671f9f0af6c476ed36975af7ff5a50d1b85efad
Kalgin Stanislav - notebook <stanislavkalgin@gmail.com>
2020-03-18 17:27:32 +0300

    nothing interesting

---
+++ Large_class/primititve_obsession/introduce_parameter_object.py
@@ -0,0 +1,17 @@
+# before
+class Order:
+    def __init__(self, customer_id, date, time, purch_goods, prices, price_coefficients, amount):
+        self.customer_id = customer_id
+        self.date = date
+        self.time = time
+        self.purch_goods = purch_goods
+        self.prices = prices
+        self.price_coefficients = price_coefficients
+        self.amount = amount
+
+    def get_customer_price(self):
+
+
+
+
+#after
